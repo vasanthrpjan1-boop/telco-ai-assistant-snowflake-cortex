24bf3f30bf42688752073304bbf97861bf9d8501
becky.oconnor <becky.oconnor@snowflake.com>
2025-03-05 17:07:40 +0000

    add_docai

--- dataops/event/deploy_documentai.template.sql
+++ dataops/event/deploy_documentai.template.sql
@@ -1,15 +1,15 @@
-use role {{ env.EVENT_ATTENDEE_ROLE }};
+use role ACCOUNTADMIN;
 
+GRANT CREATE snowflake.ml.document_intelligence on schema {{ env.DATAOPS_DATABASE }}.{{ env.DOCUMENT_AI }} to role {{EVENT_ATTENDEE_ROLE}};
+use role {{ env.EVENT_ATTENDEE_ROLE }};
 CREATE STAGE {{ env.DATAOPS_DATABASE }}.{{ env.DOCUMENT_AI }}.analyst_reports
   DIRECTORY = (enable = true)
   ENCRYPTION = (type = 'snowflake_sse');
 
-PUT file:///{{ env.CI_PROJECT_DIR }}/dataops/event/document_ai/fake_analyst_reports/*.pdf @{{ env.DATAOPS_DATABASE }}.{{ env.DOCUMENT_AI }}.analyst_reports auto_compress = false overwrite = true;
-
-CREATE STAGE RESEARCH_ANALYSTS_REPORTS.{{ env.DOCUMENT_AI }}.infographics
+CREATE STAGE {{ env.DATAOPS_DATABASE }}.{{ env.DOCUMENT_AI }}.infographics
   DIRECTORY = (enable = true)
   ENCRYPTION = (type = 'snowflake_sse');
 
-
+PUT file:///{{ env.CI_PROJECT_DIR }}/dataops/event/document_ai/fake_analyst_reports/*.pdf @{{ env.DATAOPS_DATABASE }}.{{ env.DOCUMENT_AI }}.analyst_reports auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/dataops/event/document_ai/snowflake_infographics/*.pdf @{{ env.DATAOPS_DATABASE }}.{{ env.DOCUMENT_AI }}.infographics auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/dataops/event/document_ai/snowflake_infographics/*.png @{{ env.DATAOPS_DATABASE }}.{{ env.DOCUMENT_AI }}.infographics auto_compress = false overwrite = true;
